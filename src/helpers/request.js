6b7f130cfadce57705deb05e6ee470cc63a319d4
bondiano <batbondik0@gmail.com>
2017-11-16 22:40:14 +0700

    some typo (check is token valid)

--- src/helpers/request.js
+++ src/helpers/request.js
@@ -3,17 +3,44 @@ import { API_URL } from '../constants';
 const jwt = require('webcrypto-jwt');
 
 const isValidToken = (token) => {
-    const decryped =  JSON.parse(jwt.decodeJWT(token));
-    return decryped.exp - 3600 >= Math.floor(Date.now() / 1000);
+    const re = /[a-z0-9A-Z]{3,}\.[a-z0-9A-Z]{3,}\.[a-z0-9A-Z]{3,}/g
+    if(!token.match(re)){
+        return false;
+    }
+    const exp =  JSON.parse(jwt.decodeJWT(token)).exp;
+    if(exp - 3600 >= Math.floor(Date.now() / 1000)){
+        return true;
+    }
+    if(exp - 3600 < Math.floor(Date.now() / 1000)){
+            /* TODO: REWORK IT!!! */
+        if(!window.localStorage.getItem('token_refresh').match(re)){
+            return false;
+        }
+        const config = {
+            token_refresh: window.localStorage.getItem('token_refresh')
+        };
+        url = `${API_URL}/auth/refresh-token`;
+        const headers = {
+            'Content-Type': 'application/json',
+        };
+        axios(Object.assign({}, config, { url, headers })).then(
+            (response) => {
+                return response.data;
+            },
+            (responseWithError) =>{
+                return responseWithError;
+        }); 
+        return true
+    }
 }
 
 function send(config) {
     const url = `${API_URL}${config.url}`;
-    const tokenAccess = localStorage.getItem('token_access');
+    const tokenAccess = window.localStorage.getItem('token_access');
     let headers = {
         'Content-Type': 'application/json',
     };
-
+    
     if(isValidToken(tokenAccess)){
         headers = {
             'Content-Type': 'application/json',
@@ -21,12 +48,15 @@ function send(config) {
         };
     };
     
-    
     return axios(Object.assign({}, config, { url, headers })).then(
         (response) => {
             return response.data;
         },
-        (responseWithError) => responseWithError);
+        (responseWithError) =>{
+            console.log(responseWithError)
+            return responseWithError;
+        }); 
+
 
 }
 
