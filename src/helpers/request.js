931e6acaf5015692662c350bef4818d0c76ebdce
bondiano <batbondik0@gmail.com>
2017-11-17 23:26:15 +0700

    some typo

--- src/helpers/request.js
+++ src/helpers/request.js
@@ -3,6 +3,9 @@ import { API_URL } from '../constants';
 const jwt = require('webcrypto-jwt');
 
 const isValidToken = (token) => {
+    if(!token){
+        return false;
+    }
     const re = /[a-z0-9A-Z]{3,}\.[a-z0-9A-Z]{3,}\.[a-z0-9A-Z]{3,}/g
     if(!token.match(re)){
         return false;
@@ -12,14 +15,12 @@ const isValidToken = (token) => {
         return true;
     }
     if(exp - 3600 < Math.floor(Date.now() / 1000)){
-            /* TODO: REWORK IT!!! */
-        if(!window.localStorage.getItem('token_refresh').match(re)){
-            return false;
-        }
+        /* TODO: REWORK IT!!! */
         const config = {
-            token_refresh: window.localStorage.getItem('token_refresh')
+            token_refresh: token,
+            method: 'POST',
         };
-        url = `${API_URL}/auth/refresh-token`;
+        const url = `${API_URL}/auth/refresh-token`;    
         const headers = {
             'Content-Type': 'application/json',
         };
@@ -28,6 +29,7 @@ const isValidToken = (token) => {
                 return response.data;
             },
             (responseWithError) =>{
+                console.log(responseWithError);
                 return responseWithError;
         }); 
         return true
@@ -48,12 +50,13 @@ function send(config) {
         };
     };
     
+    
     return axios(Object.assign({}, config, { url, headers })).then(
         (response) => {
             return response.data;
         },
         (responseWithError) =>{
-            console.log(responseWithError)
+            console.log(responseWithError);
             return responseWithError;
         }); 
 
