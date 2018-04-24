04ec7c35d24a41a1ba554a836fe12ae7d336aa0e
Vassiliy Kuzenkov <vassiliy.kuzenkov@noveogroup.com>
2018-04-24 15:27:34 +0700

    Calculate logo width by size

--- src/components/shared/Logo.js
+++ src/components/shared/Logo.js
@@ -1,16 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import logo from '../../assets/images/logo_circlel.png';
 
-class Logo extends React.PureComponent {
-    render() {
-        return (
-            <Link className="menu-link" to="/">
-                <img src={ logo } className="Logo" title="На главную" alt="Logo" id="logo"/>
-            </Link>
-        );
-    }
+/**
+ * @param {"small" || "medium" || "big"} size
+ */
+const getWidth = (size) => ({
+    'small': '200px',
+    'medium': '400px',
+    'big': '600px'
+})[size];
+
+function Logo({size}) {
+    return(
+        <Link to="/">
+            <img src={ logo } title="На главную" alt="Logo" id="logo" width={getWidth(size)}/>
+        </Link>
+    );
 }
 
+Logo.defaultProps = {
+    size: 'medium'
+};
+
+Logo.propTypes = {
+    size: PropTypes.oneOf(['small', 'medium', 'big'])
+};
 
 export default Logo;
